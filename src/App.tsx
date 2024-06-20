8f4a3405cd413aa34cf13b9fa868d11d916fa395
ymohit1603 <mohityadav0330@gmail.com>
2024-06-20 21:53:25 +0530

    added home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Signup from "./pages/Signup"
 import Signin from "./pages/Signin"
+import { NavBar } from "./components/NavBar";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
+        <Route path="/" element={<NavBar />}/>
           <Route path="/signup" element={<Signup />}/>
           <Route path="/signin" element={ <Signin/>} />
         </Routes>
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+import { NavBar } from "../components/NavBar"
+
+
+export const HomePage = () => {
+    return <div>
+        <div><NavBar/></div>
+    </div>
+}
