78efe4cfe281689c0a4137f5a371281498131c30
ymohit1603 <mohityadav0330@gmail.com>
2024-06-21 15:49:17 +0530

    HomePage page styling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import { Intro } from '../components/Intro';
 import { NavBar } from '../components/NavBar';
+import { PlantCategories } from '../components/PlantCategories';
 
 export const HomePage = () => {
     return (
         <div>
             <NavBar />
             <Intro />
+            <PlantCategories/>
         </div>
     );
 };
