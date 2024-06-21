03b2dbc57236715f271226e1803e5a5ae5077b67
ymohit1603 <mohityadav0330@gmail.com>
2024-06-21 22:42:36 +0530

    Added tailwind styling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,24 @@
 import { Intro } from '../components/Intro';
 import { NavBar } from '../components/NavBar';
 import { PlantCategories } from '../components/PlantCategories';
+import { PlantMedicines } from '../components/PlantMedicines';
+
+const MedicineValues = 
+{
+    title: "Buy Fertilizers",
+    description: "Discover our selection of plant-based medicines and supplements to support your health and wellness.",
+    imageUrl: "https://via.placeholder.com/150",
+    buttonText:"Explore Fertilizers",
+}
+
 
 export const HomePage = () => {
     return (
         <div>
             <NavBar />
             <Intro />
-            <PlantCategories/>
+            <PlantCategories />
+            <PlantMedicines title={MedicineValues.title} description={MedicineValues.description} imageUrl={MedicineValues.imageUrl} buttonText={MedicineValues.buttonText}/>
         </div>
     );
 };
