a7e0dfa5dfd8449cd3d14b07bd69369e04d472a9
ymohit1603 <mohityadav0330@gmail.com>
2024-06-22 23:11:35 +0530

    Backend call to get all plants

--- src/pages/Plants.tsx
+++ src/pages/Plants.tsx
@@ -1,9 +1,46 @@
-import { NavBar } from "../components/NavBar/NavBar"
-import { BuyPlantCard } from "../components/PlantCard/BuyPlantCard"
+import  { useState, useEffect } from "react";
+import axios from "axios";
+import { NavBar } from "../components/NavBar/NavBar";
+import { BuyPlantCard } from "../components/PlantCard/BuyPlantCard";
 
-export const Plants = () => {
-    return <div>
-        <NavBar />
-        <BuyPlantCard imgUrl={""} name={""} price={0} discountedPrice={0} cartButton={""} discount={0}/>
-    </div>
+interface Plant {
+    imgUrl: string;
+    name: string;
+    price: number;
+    discountedPrice: number;
+    cartButton: string;
+    discount: number;
 }
+export const Plants = () => {
+    const [plants, setPlants] = useState<Plant[]>([]);
+
+    useEffect(() => {
+        const fetchPlantsData = async () => {
+            try {
+                const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/getPlants`);
+                setPlants(response.data);
+            } catch (error) {
+                console.error("Error fetching plant data:", error);
+            }
+        };
+
+        fetchPlantsData();
+    }, []);
+
+    return (
+        <div>
+            <NavBar />
+            {plants.map((plant, index) => (
+                <BuyPlantCard 
+                    key={index}
+                    imgUrl={plant.imgUrl}
+                    name={plant.name}
+                    price={plant.price}
+                    discountedPrice={plant.discountedPrice}
+                    cartButton={plant.cartButton}
+                    discount={plant.discount}
+                />
+            ))}
+        </div>
+    );
+};
