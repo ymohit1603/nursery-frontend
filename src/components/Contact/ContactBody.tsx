5d6d1e928b8c4f97efbccb516e7b65ed6d0f277d
ymohit1603 <mohityadav0330@gmail.com>
2024-06-25 23:29:47 +0530

    Contact us component added

--- src/components/Contact/ContactBody.tsx
+++ src/components/Contact/ContactBody.tsx
@@ -1,8 +1,64 @@
+import React from 'react';
+import ContactCard from './contactCard';
 
- const ContactBody = () => {
-    return <div>
+interface ContactTypes {
+    title: string;
+    description: string;
+    bottomDescription: string;
+}
 
-    </div>
- }
+interface ContactCardType {
+    iconType: number;
+    title: string;
+    description: string;
+    val: string;
+}
+
+const cardValues: ContactCardType[] = [
+    {
+        iconType: 1,
+        title: "Email Us",
+        description: "Send us an email",
+        val: "mohityadav0330@gmail.com",
+    },
+    {
+        iconType: 2,
+        title: "Call or Text Us",
+        description: "Call or text us directly",
+        val: "+91 9050214638",
+    }
+];
+
+const ContactBody: React.FC<ContactTypes> = ({ title, description, bottomDescription }) => {
+    return (
+       <div className="w-screen h-screen mx-auto mt-5 p-6 rounded-lg shadow-md">
+          <div className="flex justify-center">
+            <div className=" max-w-2xl items-center text-center mb-4">
+                <div className="text-5xl font-bold mb-5 ">{title}</div>
+                <div className="text-gray-600">{description}</div>
+             </div>
+             </div>
+          <div className='flex flex-row mt-5'>
+            <div className="max-w-lg space-y-4">
+                  {cardValues.map((values, index) => (
+                     <ContactCard
+                           key={index}
+                           iconType={values.iconType}
+                           title={values.title}
+                           description={values.description}
+                           val={values.val}
+                     />
+                  ))}
+             </div>
+             <div>
+                
+             </div>
+          </div>
+          <div className='flex justify-center mt-8'>
+            <div className="text-center px-5 py-2 border rounded-md border-gray-300 w-fit static  text-gray-500">{bottomDescription}</div>
+          </div>
+        </div>
+    );
+};
 
 export default ContactBody;
--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -2,8 +2,17 @@ import ContactBody  from "../../components/Contact/ContactBody"
 import Layout from "../../components/Layout"
 
 
+const contactVal =
+    {
+    title: `We are here for you,
+        contact us anytime`,
+        description: "Have any questions about our services or just want to talk with us?Please reach out.",
+        bottomDescription:"We'll get back to you as soon as possible.We are available 8am-6pm on weekdays."
+    }
+
+
 export const ContactUs = () => {
     return <Layout>
-        <ContactBody/>
+        <ContactBody title={contactVal.title} description={contactVal.description} bottomDescription={contactVal.bottomDescription}/>
         </Layout>
 }
