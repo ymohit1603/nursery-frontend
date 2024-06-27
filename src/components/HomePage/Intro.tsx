ff684015cc78967c5705cca93639a0a890681e28
ymohit1603 <mohityadav0330@gmail.com>
2024-06-27 16:28:18 +0530

    Fix: tailwind config error

--- src/components/HomePage/Intro.tsx
+++ src/components/HomePage/Intro.tsx
@@ -8,16 +8,18 @@ export const Intro = () => {
         navigate('/plants');
     }
     return (
-        <div className="p-4 flex flex-col md:flex-row items-center md:items-start m-10 mt-16 mb-32 md:h-screen max-h-96">
-            <div className=" ml-8 p-3 md:mr-5 ">
-                <div className="text-5xl font-bold mb-6">Discover the Perfect Plants for Your Home</div>
-                <div className="text-xl mb-3 text-slate-600">Browse our curated selection of lush, healthy plants and connect with your nearest plantly verified nursery to find the ideal greenery for your space.</div>
-                <div className="flex space-x-4 mb-6 mt-10">
-                    <button className="bg-black w-40 text-white py-2 px-4 rounded-md hover:bg-slate-800" onClick={HandleBrowsePlant}>Browse Plants</button>
-                    <button className="bg-black w-40 text-white py-2 px-4 rounded-md hover:bg-slate-800">Browse Nurseries</button>
+        <div className='flex justify-center items-center h-screen ml-16'>
+             <div className="flex flex-row">
+                <div className=" ml-8 p-3 md:mr-5 ">
+                    <div className="text-5xl font-bold mb-6">Discover the Perfect Plants for Your Home</div>
+                    <div className="text-xl mb-3 text-slate-600">Browse our curated selection of lush, healthy plants and connect with your nearest plantly verified nursery to find the ideal greenery for your space.</div>
+                    <div className="flex space-x-4 mb-6 mt-10">
+                        <button className="bg-black w-40 text-white py-2 px-4 rounded-md hover:bg-slate-800" onClick={HandleBrowsePlant}>Browse Plants</button>
+                        <button className="bg-black w-40 text-white py-2 px-4 rounded-md hover:bg-slate-800">Browse Nurseries</button>
+                    </div>
                 </div>
-            </div>
-            <img src={homeNurseryImage} alt="Nursery" className="w-full max-h-80 object-contain rounded" />
+                <img src={homeNurseryImage} alt="Nursery" className="w-full max-h-80 object-contain rounded" />
         </div>
+       </div>
     );
 };
