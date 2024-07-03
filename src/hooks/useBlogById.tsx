85231bd00d81a8e8ece4764ced1052ee26279c3a
ymohit1603 <mohityadav0330@gmail.com>
2024-07-03 19:32:40 +0530

    Refactor: Api call for specific blog with id

---
+++ src/hooks/useBlogById.tsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+
+export const useBlogById = ({ endpoints }: { endpoints:string }) => {
+    const [blog, setBlog] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+        useEffect(() => {
+            const fetchBlog = async () => {
+                try {
+                    const result = await axios.get(`${process.env.VITE_BACKEND_URL}/${endpoints}`);
+                    setBlog(result.data);
+                } catch (error) {
+                    setError('Error fetching blog');
+                } finally {
+                    setLoading(false);
+                }
+            };
+    
+            fetchBlog();
+        }, [endpoints]);
+    
+
+    return { blog, loading, error };
+
+}
