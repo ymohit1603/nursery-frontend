79f041d117d934a0e793128bb5b366ae976b7185
ymohit1603 <mohityadav0330@gmail.com>
2024-07-03 19:14:29 +0530

    Add: specific blogs of different categories

---
+++ src/pages/Blog/indoor/byId.tsx
@@ -0,0 +1,10 @@
+import Layout from "../../../components/Layout";
+import IndoorPlantById from "../../../components/Blog/specific/byId"
+
+const SpecificIndoorPlant = () => {
+    return <Layout>
+        <IndoorPlantById/>
+    </Layout >
+}
+
+export default SpecificIndoorPlant;
