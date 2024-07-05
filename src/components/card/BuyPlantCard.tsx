4fc339bf6e71a943fa7575012d87f524e7ce56bb
ymohit1603 <mohityadav0330@gmail.com>
2024-07-05 21:24:23 +0530

    Refactor:tailwind styling

--- src/components/card/BuyPlantCard.tsx
+++ src/components/card/BuyPlantCard.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
 interface BuyPlantCardTypes {
+    plantId:number,
     imgUrl: string;
     name: string;
     price: number;
@@ -9,16 +10,16 @@ interface BuyPlantCardTypes {
     discount: number;
 }
 
-export const BuyPlantCard: React.FC<BuyPlantCardTypes> = ({ imgUrl, name, discountedPrice, price, cartButton, discount }) => {
+export const BuyPlantCard: React.FC<BuyPlantCardTypes> = ({ plantId,imgUrl, name, discountedPrice, price, cartButton, discount }) => {
     return <div className="relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
-            <Link className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" to="#">
+            <Link className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" to={`/plant/:${plantId}`}>
                 <img className="object-cover" src={imgUrl} alt="product image" />
                 <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">
                     {discount}% OFF
                 </span>
             </Link>
             <div className="mt-4 px-5 pb-5">
-                <Link to="#">
+                <Link to={`/plant/:${plantId}`}>
                     <h5 className="text-xl tracking-tight text-slate-900">{name}</h5>
                 </Link>
                 <div className="mt-2 mb-5 flex items-center justify-between">
@@ -45,7 +46,7 @@ export const BuyPlantCard: React.FC<BuyPlantCardTypes> = ({ imgUrl, name, discou
                         <span className="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">5.0</span>
                     </div>
                 </div>
-                <Link to="#" className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
+                <Link to={`/plant/:${plantId}`} className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
                     <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 h-6 w-6" fill="none"> </svg>
                 {cartButton }</Link >
     </div>
