efbc059631154bbf45586d8c4d0d7030c0b62ac5
ymohit1603 <mohityadav0330@gmail.com>
2024-07-10 22:06:15 +0530

    provider for redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import  { Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 
 const Plants = lazy(() => import('./pages/Plants'));
@@ -19,27 +21,27 @@ import Signin from './pages/Auth/Signin';
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/signin" element={<Signin />} />
-            <Route path="/plants" element={<Plants />} />
-            <Route path="/plant/:id" element={<PlantsById />} />
-            <Route path="/medicines" element={<Medicines />} />
-            <Route path="/contact" element={<ContactUs />} />
-            <Route path="/blog" element={<Blogs />} />
-            <Route path="/blog/indoor" element={<IndoorPlant />} />
-            <Route path="/blog/indoor/:id" element={<SpecificIndoorPlant />} />
-            <Route path="/blog/outdoor" element={<OutdoorPlant />} />
-            <Route path="/blog/other" element={<OtherPlant />} />
-            <Route path="/cart" element={<Cart />} />
-          </Routes>
-        </Suspense>
-      </BrowserRouter>
-    </div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/signin" element={<Signin />} />
+              <Route path="/plants" element={<Plants />} />
+              <Route path="/plant/:id" element={<PlantsById />} />
+              <Route path="/medicines" element={<Medicines />} />
+              <Route path="/contact" element={<ContactUs />} />
+              <Route path="/blog" element={<Blogs />} />
+              <Route path="/blog/indoor" element={<IndoorPlant />} />
+              <Route path="/blog/indoor/:id" element={<SpecificIndoorPlant />} />
+              <Route path="/blog/outdoor" element={<OutdoorPlant />} />
+              <Route path="/blog/other" element={<OtherPlant />} />
+              <Route path="/cart" element={<Cart />} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </Provider>
   );
 }
 
