9353a75b1bc2860747a2e42b05ffc780915292e7
ymohit1603 <mohityadav0330@gmail.com>
2024-07-17 16:18:57 +0530

    fix

--- src/pages/findNursery/index.tsx
+++ src/pages/findNursery/index.tsx
@@ -1,8 +1,25 @@
-
+import NurseryCard from "../../components/card/NurseryCard";
+import { useAppSelector } from "../../redux/hook";
 
 const Nursery = () => {
+
+    const nearby = useAppSelector((state) => state.nursery.nurseries);
+    const status = useAppSelector((state) => state.nursery.status);
+    const error = useAppSelector((state) => state.nursery.error);
+
+
     return <div>
-        Hey this is your Nursery
+        <div>
+        {status === 'loading' && <p>Loading...</p>}
+        {status === 'failed' && <p>Error: {error}</p>}
+        {status === 'succeeded' &&
+            nearby.map((nursery) => (
+                <div>
+                    <NurseryCard name={nursery.name} location={nursery.location} ratings={nursery.ratings} timing={nursery.timings} />
+                </div>
+            ))}
+        </div>
     </div>
+    
 }
 export default Nursery;
