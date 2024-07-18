7d2775c9204160a1fc580d8f312fabb68fb91a56
ymohit1603 <mohityadav0330@gmail.com>
2024-07-18 14:10:56 +0530

    Add: navigation to medicine route on button click

--- src/components/AllMeds/index.tsx
+++ src/components/AllMeds/index.tsx
@@ -1,48 +1,48 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { BuyPlantCard } from "../card/BuyPlantCard";
-import axios from "axios";
+// import axios from "axios";
 
-interface medicineTypes{
-    imgUrl: string,
-    name: string,
-    price: number,
-    discountedPrice: number,
-    cartButton: string,
-    discount:number
-}
+// interface medicineTypes{
+//     imgUrl: string,
+//     name: string,
+//     price: number,
+//     discountedPrice: number,
+//     cartButton: string,
+//     discount:number
+// }
 
 
 
 export const AllMedicines = () => {
 
-    const [medicines, setMedicines] = useState<medicineTypes[]>([]);
+    // const [medicines, setMedicines] = useState<medicineTypes[]>([]);
 
-    useEffect(() => {
-        const fetchMedicines = async () => {
-            try {
-                const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/medicines`);
-                setMedicines(response.data);
-            } catch (error) {
-                console.error("Error fetching medicines:", error);
-            }
-        };
+    // useEffect(() => {
+    //     const fetchMedicines = async () => {
+    //         try {
+    //             const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/medicines`);
+    //             setMedicines(response.data);
+    //         } catch (error) {
+    //             console.error("Error fetching medicines:", error);
+    //         }
+    //     };
 
-        fetchMedicines();
-    }, []);
+    //     fetchMedicines();
+    // }, []);
 
     return <div>
         <div>
-             {medicines.map((med, index) => (
+             {/* {medicines.map((med, index) => (
                 <BuyPlantCard 
-                    key={index}
-                    imgUrl={med.imgUrl}
-                    name={med.name}
-                    price={med.price}
-                    discountedPrice={med.discountedPrice}
-                    cartButton={med.cartButton}
-                    discount={med.discount}
-                />
-            ))}
+                     key={index}
+                     imgUrl={med.imgUrl}
+                     name={med.name}
+                     price={med.price}
+                     discountedPrice={med.discountedPrice}
+                     cartButton={med.cartButton}
+                     discount={med.discount} plantId={0}                />
+            ))} */}
+            <BuyPlantCard plantId={0} imgUrl={""} name={""} price={0} discountedPrice={0} cartButton={""} discount={0} />
         </div>
     </div>
 }
