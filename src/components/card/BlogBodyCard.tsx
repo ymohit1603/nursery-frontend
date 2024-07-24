83c313ec91a30dececd6e8e1d4046156333c3109
ymohit1603 <mohityadav0330@gmail.com>
2024-07-24 14:40:54 +0530

    feat: user can like blog

--- src/components/card/BlogBodyCard.tsx
+++ src/components/card/BlogBodyCard.tsx
@@ -2,7 +2,15 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import { Button, CardActionArea, CardActions, IconButton, } from '@mui/material';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch } from '../../redux/hook';
+import { decrement, increase } from '../../redux/slices/blogSlice';
 
 
 interface cardTypes{
@@ -12,30 +20,58 @@ interface cardTypes{
   Description:string
 }
 
+export default function BlogCard({ imgUrl, title, Description }: cardTypes) {
+  const [liked, setLiked] = useState(false);
+  const [bookmarked, setBookmarked] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
-export default function BlogCard({imgUrl,title,Description }:cardTypes) {
+  const handleLike = () => {
+    if (liked) {
+      dispatch(decrement());
+    }
+    else {
+      dispatch(increase());
+    }
+    setLiked(!liked);
+  };
+
+  const handleBookmark = () => {
+    setBookmarked(!bookmarked);
+  };
+
+  const handleCardClick = () => {
+    navigate(':id')
+  }
+
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          className='bg-gray-400 h-48'
-          component="img"
-          image={imgUrl}
-          alt={`${title} image`}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-           {Description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
+      <div onClick={handleCardClick}>
+        <CardActionArea>
+          <CardMedia
+            className='bg-gray-400 h-48'
+            component="img"
+            image={imgUrl}
+            alt={`${title} image`}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+            {Description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </div>
+        <CardActions>
+        <Button onClick={handleCardClick} size="small" color="primary">
           Read more
         </Button>
+        <div style={{ marginLeft: 'auto' }}>
+            <IconButton onClick={handleLike}>{liked ? <FavoriteIcon color="secondary" /> : <FavoriteBorderIcon />}</IconButton>        
+            <IconButton onClick={handleBookmark}>{bookmarked ? <BookmarkIcon color="secondary" /> : <BookmarkBorderIcon />}</IconButton>
+        </div>
       </CardActions>
     </Card>
   );
