2356670bbe84a2515b6b7670fb75f918d22f9bb0
ymohit1603 <mohityadav0330@gmail.com>
2024-08-07 00:06:39 +0530

    fix: state uplifting for both category and price

--- src/components/filters/prices.tsx
+++ src/components/filters/prices.tsx
@@ -1,28 +1,37 @@
 import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material"
-import { SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction } from "react"
 
+interface priceTypes{
+  value: string | null
+  setValue: Dispatch<SetStateAction<string | null>>
+}
 
-const PriceFilter = () => {
+const PriceFilter = ({value,setValue}:priceTypes) => {
 
-    const [value, setValue] = useState('');
-
-    const handleChange = (e: { target: { value: SetStateAction<string>; }; }) => {
-        setValue(e.target.value);
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newValue = e.target.value;
+      setValue(newValue);
+    }
+  
+    const handleClick = (newValue: string) => {
+      if (value === newValue) {
+        setValue(null);
+      }
     }
 
-
-    return <FormControl>
+  return (<FormControl>
     <FormLabel id="demo-controlled-radio-buttons-group">Price</FormLabel>
     <RadioGroup
       aria-labelledby="demo-controlled-radio-buttons-group"
       name="controlled-radio-buttons-group"
       value={value}
       onChange={handleChange}
+        
     >
-      <FormControlLabel value="ascending" control={<Radio />} label="Low to High" />
-      <FormControlLabel value="descending" control={<Radio />} label="High to Low" />
+      <FormControlLabel value="ascending" control={<Radio onClick={() => handleClick("ascending")} />} label="Low to High" />
+      <FormControlLabel value="descending" control={<Radio onClick={() => handleClick("descending")} />} label="High to Low" />
     </RadioGroup>
-  </FormControl>
+  </FormControl>);
 }
 
 export default PriceFilter;
